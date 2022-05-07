5870f859279df3fec0c9c11880874a6ae982fd2e
Matchobas <maalexandria@hotmail.com>
2022-05-07 15:04:38 -0300

    NLW Feedback Content & ScreenshotButton created

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,3 +1,56 @@
-export function FeedbackContentStep() {
-  return (<p></p>);
+import { ArrowLeft, Camera } from "phosphor-react";
+import { FeedbackTypes, feedbackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../../ScreenshotButton";
+
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackTypes;
+  onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  return (
+    <>
+      <header>
+        <button 
+          type="button" 
+          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+          onClick={onFeedbackRestartRequested}
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />  
+        </button>
+
+        <span className="text-xl leading-6 flex items-center gap-2">
+          <img 
+            src={feedbackTypeInfo.image.source}
+            alt={feedbackTypeInfo.image.alt}
+            className="w-6 h-6" 
+          />
+          {feedbackTypeInfo.title}
+        </span>
+
+        <CloseButton />
+      </header>
+      
+      <form className="my-4 w-full">
+        <textarea 
+          className="min-w-[19rem] w-full min-h-[7rem] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 ring-1 focus:outline-none"
+          placeholder="Conte o que está acontecendo com detalhes..."
+        />
+
+        <footer className="flex gap-2 mt-2">
+          <ScreenshotButton />
+
+          <button 
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Enviar
+          </button>
+        </footer>
+      </form>
+    </>
+  );
 };
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,7 +1,7 @@
-import { CloseButton } from '../CloseButton';
-import bugImage from '../assets/Bug.svg';
-import ideaImage from '../assets/Idea.svg';
-import thoughtImage from '../assets/Thought.svg';
+import bugImage from '../../assets/Bug.svg';
+import ideaImage from '../../assets/Idea.svg';
+import thoughtImage from '../../assets/Thought.svg';
+
 import { useState } from 'react';
 import { FeedbackTypeStep } from './Steps/FeedbackTypeStep';
 import { FeedbackContentStep } from './Steps/FeedbackContentStep';
@@ -35,20 +35,24 @@ export type FeedbackTypes = keyof typeof feedbackTypes;
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackTypes | null>(null);
 
+  function onFeedbackTypeRestart() {
+    setFeedbackType(null);
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-          
+
 
       {!feedbackType ? (
           <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
         ) : (
-          <FeedbackContentStep />
+          <FeedbackContentStep feedbackType={feedbackType} onFeedbackRestartRequested={onFeedbackTypeRestart} />
         )
       }
 
       <footer className="text-xs text-neutral-400">
         Feito com carinho S2 por 
-        <a className="underline underline-offset-1" href="https://github.com/Matchobas/" target="_blank">Matheus</a>
+        <a className="underline underline-offset-1" href="https://github.com/Matchobas/" target="_blank"> Matheus</a>
       </footer>
     </div>
   );
